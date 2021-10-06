ce2f765c69c8142880e0b0167fb1753c32cadf0e
Andy Blackledge <andyblackledge@hotmail.com>
2021-10-06 19:53:49 +0100

    Initial commit

---
+++ RoM2UI/MainForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace RoM2UI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.statusStrip = new System.Windows.Forms.StatusStrip();
+			this.lastSavedLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.mainTabControl = new System.Windows.Forms.TabControl();
+			this.Time = new System.Windows.Forms.TabPage();
+			this.timeTextBox = new System.Windows.Forms.TextBox();
+			this.Notes = new System.Windows.Forms.TabPage();
+			this.notesTextBox = new System.Windows.Forms.TextBox();
+			this.Report = new System.Windows.Forms.TabPage();
+			this.reportTextBox = new System.Windows.Forms.TextBox();
+			this.saveTimer = new System.Windows.Forms.Timer(this.components);
+			this.statusStrip.SuspendLayout();
+			this.mainTabControl.SuspendLayout();
+			this.Time.SuspendLayout();
+			this.Notes.SuspendLayout();
+			this.Report.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// statusStrip
+			// 
+			this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lastSavedLabel});
+			this.statusStrip.Location = new System.Drawing.Point(0, 474);
+			this.statusStrip.Name = "statusStrip";
+			this.statusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+			this.statusStrip.Size = new System.Drawing.Size(737, 22);
+			this.statusStrip.TabIndex = 1;
+			this.statusStrip.Text = "statusStrip1";
+			// 
+			// lastSavedLabel
+			// 
+			this.lastSavedLabel.Name = "lastSavedLabel";
+			this.lastSavedLabel.Size = new System.Drawing.Size(102, 17);
+			this.lastSavedLabel.Text = "Last Saved: HH:mm";
+			// 
+			// mainTabControl
+			// 
+			this.mainTabControl.Controls.Add(this.Time);
+			this.mainTabControl.Controls.Add(this.Notes);
+			this.mainTabControl.Controls.Add(this.Report);
+			this.mainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.mainTabControl.Location = new System.Drawing.Point(0, 0);
+			this.mainTabControl.Margin = new System.Windows.Forms.Padding(2);
+			this.mainTabControl.Name = "mainTabControl";
+			this.mainTabControl.SelectedIndex = 0;
+			this.mainTabControl.Size = new System.Drawing.Size(737, 474);
+			this.mainTabControl.TabIndex = 2;
+			// 
+			// Time
+			// 
+			this.Time.Controls.Add(this.timeTextBox);
+			this.Time.Location = new System.Drawing.Point(4, 22);
+			this.Time.Margin = new System.Windows.Forms.Padding(2);
+			this.Time.Name = "Time";
+			this.Time.Padding = new System.Windows.Forms.Padding(2);
+			this.Time.Size = new System.Drawing.Size(729, 448);
+			this.Time.TabIndex = 0;
+			this.Time.Text = "Time";
+			this.Time.UseVisualStyleBackColor = true;
+			// 
+			// timeTextBox
+			// 
+			this.timeTextBox.AcceptsReturn = true;
+			this.timeTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.timeTextBox.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.timeTextBox.Location = new System.Drawing.Point(2, 2);
+			this.timeTextBox.Margin = new System.Windows.Forms.Padding(2);
+			this.timeTextBox.Multiline = true;
+			this.timeTextBox.Name = "timeTextBox";
+			this.timeTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.timeTextBox.Size = new System.Drawing.Size(725, 444);
+			this.timeTextBox.TabIndex = 0;
+			this.timeTextBox.WordWrap = false;
+			this.timeTextBox.TextChanged += new System.EventHandler(this.textChanged);
+			this.timeTextBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.timeTextBox_KeyDown);
+			// 
+			// Notes
+			// 
+			this.Notes.Controls.Add(this.notesTextBox);
+			this.Notes.Location = new System.Drawing.Point(4, 22);
+			this.Notes.Margin = new System.Windows.Forms.Padding(2);
+			this.Notes.Name = "Notes";
+			this.Notes.Padding = new System.Windows.Forms.Padding(2);
+			this.Notes.Size = new System.Drawing.Size(729, 448);
+			this.Notes.TabIndex = 1;
+			this.Notes.Text = "Notes";
+			this.Notes.UseVisualStyleBackColor = true;
+			// 
+			// notesTextBox
+			// 
+			this.notesTextBox.AcceptsReturn = true;
+			this.notesTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.notesTextBox.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.notesTextBox.Location = new System.Drawing.Point(2, 2);
+			this.notesTextBox.Margin = new System.Windows.Forms.Padding(2);
+			this.notesTextBox.Multiline = true;
+			this.notesTextBox.Name = "notesTextBox";
+			this.notesTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.notesTextBox.Size = new System.Drawing.Size(725, 444);
+			this.notesTextBox.TabIndex = 1;
+			this.notesTextBox.WordWrap = false;
+			this.notesTextBox.TextChanged += new System.EventHandler(this.textChanged);
+			// 
+			// Report
+			// 
+			this.Report.Controls.Add(this.reportTextBox);
+			this.Report.Location = new System.Drawing.Point(4, 22);
+			this.Report.Margin = new System.Windows.Forms.Padding(2);
+			this.Report.Name = "Report";
+			this.Report.Padding = new System.Windows.Forms.Padding(2);
+			this.Report.Size = new System.Drawing.Size(729, 448);
+			this.Report.TabIndex = 2;
+			this.Report.Text = "Report";
+			this.Report.UseVisualStyleBackColor = true;
+			// 
+			// reportTextBox
+			// 
+			this.reportTextBox.AcceptsReturn = true;
+			this.reportTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.reportTextBox.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.reportTextBox.Location = new System.Drawing.Point(2, 2);
+			this.reportTextBox.Margin = new System.Windows.Forms.Padding(2);
+			this.reportTextBox.Multiline = true;
+			this.reportTextBox.Name = "reportTextBox";
+			this.reportTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.reportTextBox.Size = new System.Drawing.Size(725, 444);
+			this.reportTextBox.TabIndex = 1;
+			this.reportTextBox.WordWrap = false;
+			// 
+			// saveTimer
+			// 
+			this.saveTimer.Interval = 1000;
+			this.saveTimer.Tick += new System.EventHandler(this.saveTimer_Tick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(737, 496);
+			this.Controls.Add(this.mainTabControl);
+			this.Controls.Add(this.statusStrip);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(2);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "RoM2";
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.statusStrip.ResumeLayout(false);
+			this.statusStrip.PerformLayout();
+			this.mainTabControl.ResumeLayout(false);
+			this.Time.ResumeLayout(false);
+			this.Time.PerformLayout();
+			this.Notes.ResumeLayout(false);
+			this.Notes.PerformLayout();
+			this.Report.ResumeLayout(false);
+			this.Report.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.TabControl mainTabControl;
+        private System.Windows.Forms.TabPage Time;
+        private System.Windows.Forms.TabPage Notes;
+        private System.Windows.Forms.TabPage Report;
+        private System.Windows.Forms.ToolStripStatusLabel lastSavedLabel;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.Timer saveTimer;
+        private System.Windows.Forms.TextBox notesTextBox;
+        private System.Windows.Forms.TextBox reportTextBox;
+    }
+}
+
---
+++ RoM2UI/MainForm.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RoM2UI
+{
+	public partial class MainForm : Form
+	{
+		#region Member declarations
+
+		private const string TimeTextFileName = "Time.txt";
+		private const string NotesTextFileName = "Notes.txt";
+
+		private DateTime lastUpdateDateTime;
+		private bool isSaved;
+
+		#endregion
+
+		#region Constructors
+
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		#endregion
+
+		#region Private methods
+
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			this.timeTextBox.Text = GetFileText(MainForm.TimeTextFileName);
+			this.notesTextBox.Text = GetFileText(MainForm.NotesTextFileName);
+			this.reportTextBox.Text = GetReportText(this.timeTextBox.Text);
+
+			this.lastUpdateDateTime = DateTime.Now;
+
+			this.isSaved = true;
+			this.lastSavedLabel.Text = string.Format("Loaded");
+		}
+
+
+		private string GetFileText(string fileName)
+		{
+			if (!File.Exists(fileName))
+			{
+				File.WriteAllText(fileName, string.Empty);
+			}
+
+			var timeText = File.ReadAllText(fileName);
+
+			return timeText;
+		}
+
+
+		private void saveTimer_Tick(object sender, EventArgs e)
+		{
+			if (!this.isSaved)
+			{
+				TimeSpan unsavedTimeSpan = DateTime.Now - this.lastUpdateDateTime;
+
+				if (unsavedTimeSpan.TotalSeconds > 5)
+				{
+					SaveText();
+				}
+			}
+		}
+
+
+		private void SaveText()
+		{
+			File.WriteAllText(MainForm.TimeTextFileName, this.timeTextBox.Text);
+			File.WriteAllText(MainForm.NotesTextFileName, this.notesTextBox.Text);
+
+			this.reportTextBox.Text = GetReportText(this.timeTextBox.Text);
+
+			this.lastSavedLabel.Text = string.Format("Saved: {0:HH:mm}", DateTime.Now);
+			this.saveTimer.Stop();
+		}
+
+
+		private string GetReportText(string timeText)
+		{
+			var reportDateLines = GetReportDateLines(timeText);
+
+			Console.WriteLine(reportDateLines);
+
+			var totalHoursPerMonth = new SortedDictionary<string, double>();
+
+			var reportTextBuilder = new StringBuilder();
+
+			foreach (var reportDate in reportDateLines.Keys)
+			{
+				reportTextBuilder.AppendLine(reportDate.ToString("ddd dd/MM/yyyy"));
+				reportTextBuilder.AppendLine(reportDate.ToString("=============="));
+
+				var dateTotalHoursByDescription = new Dictionary<string, double>();
+
+				var lines = reportDateLines[reportDate];
+
+				for (int i = 0; i < lines.Count; i++)
+				{
+					if (string.IsNullOrWhiteSpace(lines[i].Item2))
+					{
+						continue;
+					}
+
+					var startTime = lines[i].Item1;
+
+					DateTime stopTime;
+
+					if (i + 1 == lines.Count)
+					{
+						if (reportDate != DateTime.Now.Date)
+						{
+							continue;
+						}
+
+						stopTime = DateTime.Now;
+					}
+					else
+					{
+						stopTime = lines[i + 1].Item1;
+					}
+
+					var time = stopTime - startTime;
+
+					if (dateTotalHoursByDescription.ContainsKey(lines[i].Item2))
+					{
+						dateTotalHoursByDescription[lines[i].Item2] = dateTotalHoursByDescription[lines[i].Item2] + time.TotalHours;
+					}
+					else
+					{
+						dateTotalHoursByDescription[lines[i].Item2] = time.TotalHours;
+					}
+				}
+
+				double dateTotalHours = 0;
+
+				foreach (var description in dateTotalHoursByDescription.Keys)
+				{
+					reportTextBuilder.AppendLine(
+						string.Format("{0:0.00}h : {1}", dateTotalHoursByDescription[description], description));
+
+                    var descriptionDateTotal = dateTotalHoursByDescription[description];
+
+                    dateTotalHours += descriptionDateTotal;
+
+					var monthlyKey = $"{reportDate.ToString("yyyy-MMM")} {description}";
+
+                    if (totalHoursPerMonth.ContainsKey(monthlyKey))
+                    {
+                        double currentTotal = totalHoursPerMonth[monthlyKey];
+                        totalHoursPerMonth[monthlyKey] = currentTotal + descriptionDateTotal;
+					}
+					else
+                    {
+						totalHoursPerMonth.Add(monthlyKey, descriptionDateTotal);
+					}
+				}
+
+				reportTextBuilder.AppendLine(
+					string.Format("{0:0.00}h : Total", dateTotalHours));
+
+				reportTextBuilder.AppendLine();
+			}
+
+            reportTextBuilder.AppendLine("Monthly totals");
+            reportTextBuilder.AppendLine("==============");
+
+            foreach (var monthKey in totalHoursPerMonth.Keys)
+            {
+				reportTextBuilder.AppendLine($"{monthKey}: {totalHoursPerMonth[monthKey]:0.00}h");
+			}
+
+			return reportTextBuilder.ToString();
+		}
+
+
+		private static Dictionary<DateTime, List<Tuple<DateTime, string>>> GetReportDateLines(string timeText)
+		{
+			string[] timeLines = timeText.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+			var reportDateLines = new Dictionary<DateTime, List<Tuple<DateTime, string>>>();
+
+			DateTime reportDate = DateTime.Now.Date;
+
+			foreach (var timeLine in timeLines)
+			{
+				DateTime lineDate;
+				if (DateTime.TryParse(timeLine, out lineDate))
+				{
+					reportDate = lineDate.Date;
+				}
+
+				var lineTimeMatch =
+					Regex.Match(timeLine, "^(?<hours>[0-2][0-9])[:]?(?<minutes>[0-5][0-9])[ ]?(?<description>.*)");
+
+				if (lineTimeMatch.Success)
+				{
+					string timeMatch =
+						lineTimeMatch.Groups["hours"].Value + ":" + lineTimeMatch.Groups["minutes"].Value;
+					string descriptionMatch =
+						lineTimeMatch.Groups["description"].Value;
+
+					DateTime time;
+					if (DateTime.TryParse(timeMatch, out time))
+					{
+						if (!reportDateLines.ContainsKey(reportDate))
+						{
+							reportDateLines.Add(reportDate, new List<Tuple<DateTime, string>>());
+						}
+
+						var reportDateLine = new Tuple<DateTime, string>(time, descriptionMatch);
+
+						reportDateLines[reportDate].Add(reportDateLine);
+					}
+				}
+			}
+			return reportDateLines;
+		}
+
+
+		private void textChanged(object sender, EventArgs e)
+		{
+			this.isSaved = false;
+			this.lastUpdateDateTime = DateTime.Now;
+			this.lastSavedLabel.Text = string.Format("Edited: {0:HH:mm}", DateTime.Now);
+
+			this.saveTimer.Start();
+		}
+
+
+		private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			this.saveTimer.Stop();
+		}
+
+
+		private void timeTextBox_KeyDown(object sender, KeyEventArgs e)
+		{
+			if ((e.Modifiers == Keys.Control) && (e.KeyCode == Keys.T))
+			{
+				var currentDateText = 
+					string.Format("================================================================================================"
+						+ "\r\n"
+						+ "\r\n{0:ddd dd/MM/yy}"
+						+ "\r\n"
+						+ "\r\n", DateTime.Now);
+				this.timeTextBox.Paste(currentDateText);
+			}
+		}
+
+		#endregion
+	}
+}
